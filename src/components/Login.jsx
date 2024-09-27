dd0d3a4febc606eb783d75546ffb6763f63367cf
Shubham Yadav <shubhamyadav97660@gmail.com>
2024-09-27 22:22:40 +0530

    feat: to show wrong password or email pop up

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,11 @@
 import { useLogin } from "../context/loginContext";
 import { getToken } from "../api/getToken";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 export const Login = () => {
   
   const {email, password, dispatchLogin} = useLogin();
+  const [showMessage, setShowMessage] = useState(false);
   const navigate = useNavigate();
 
   const handleFormSubmit = async (e) => {
@@ -16,6 +18,8 @@ export const Login = () => {
     });
     localStorage.setItem('accessToken', JSON.stringify(token));
     navigate("/");
+  } else {
+    setShowMessage(!showMessage)
   }
   };
 
@@ -37,7 +41,7 @@ export const Login = () => {
     <>
       <form
         onSubmit={handleFormSubmit}
-        className="absolute mt-20 bg-slate-300 flex flex-col border border-gray-950 p-2"
+        className="absolute mt-20 bg-slate-300 flex flex-col w-1/3 border border-gray-950 p-5 rounded-md"
       >
         <label for="email">Email*</label>
         <input type="email" onChange={handleEmail} className="m-2" id="email" />
@@ -48,12 +52,20 @@ export const Login = () => {
           className="m-2"
           id="password"
         />
+        <div>
+          Use this 
+          <p>username:
+          "john@mail.com" and</p> <p>password: "changeme"</p>
+        </div>
         <div className="flex justify-center">
           <button type="submit" className=" button btn-primary btn-icon cart-btn d-flex align-center justify-center gap cursor btn-margin">
             Login
           </button>
         </div>
       </form>
+      {showMessage ? <div className="text-white bg-red-600">
+        You have entered the wrong username or password
+      </div> : ""}
     </>
   );
 };
